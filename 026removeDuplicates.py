12ec6455237c3ba1408f15ff9b4587a4fca10f98
michaelyin <michael.yin@greyspark.com>
2018-10-03 22:25:30 +0800

    026removeDuplicates.py

---
+++ 026removeDuplicates.py
@@ -0,0 +1,47 @@
+"""
+https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+Example 1:
+
+Given nums = [1,1,2],
+
+Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+
+It doesn't matter what you leave beyond the returned length.
+Example 2:
+
+Given nums = [0,0,1,1,1,2,2,3,3,4],
+
+Your function should return length = 5, with the first five elements of nums being modified to 0, 1, 2, 3, and 4 respectively.
+
+It doesn't matter what values are set beyond the returned length.
+Clarification:
+
+Confused why the returned value is an integer but your answer is an array?
+
+Note that the input array is passed in by reference, which means modification to the input array will be known to the caller as well.
+
+Internally you can think of this:
+
+
+"""
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        length = len(nums) -1
+        i = 0
+        labels = []
+        while i<= length:
+            if nums[i] in labels:
+                nums.pop(i)
+                length = length-1
+            else:
+                labels.append(nums[i])
+                i = i+1
+        return len(nums)

48a74db5971c2e6e3ae516c775f583a94741e2a6
michaelyin <michael.yin@greyspark.com>
2018-10-05 21:49:11 +0800

    027removeElement.py

---
+++ 027removeElement.py
@@ -0,0 +1,46 @@
+"""
+Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+
+Example 1:
+
+Given nums = [3,2,2,3], val = 3,
+
+Your function should return length = 2, with the first two elements of nums being 2.
+
+It doesn't matter what you leave beyond the returned length.
+Example 2:
+
+Given nums = [0,1,2,2,3,0,4,2], val = 2,
+
+Your function should return length = 5, with the first five elements of nums containing 0, 1, 3, 0, and 4.
+
+Note that the order of those five elements can be arbitrary.
+
+It doesn't matter what values are set beyond the returned length.
+"""
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """    
+        if len(nums) == 0:
+            return 0
+        ns = len(nums)
+        i =0
+        while i< ns:
+            #print i,ns
+            if nums[i] == val:
+                nums.pop(i)
+                ns = ns -1
+            else:
+                i = i+1
+        return ns
+        
+        
+        

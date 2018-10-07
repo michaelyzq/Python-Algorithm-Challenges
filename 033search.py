621dfb84deca1224e1d1be220d0cb5525afa0b7a
michaelyin <michael.yin@greyspark.com>
2018-10-07 22:02:09 +0800

    033search.py

---
+++ 033search.py
@@ -0,0 +1,41 @@
+"""
+https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+
+You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+You may assume no duplicate exists in the array.
+
+Your algorithm's runtime complexity must be in the order of O(log n).
+
+Example 1:
+
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+
+"""
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if len(nums) == 0:
+            return -1
+        if nums[0]<= target:
+            for i in range(len(nums)):
+                if nums[i] == target:
+                    return i
+        else:
+             for i in reversed(range(len(nums))):
+                if nums[i] == target:
+                    return i
+        return -1
